7f6e377674453787e55759caa5f7007574c9b012
atharvaombase <aaombase@gmail.com>
2025-02-26 16:47:43 +0530

    Feat:Added searchBar in navbar

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
     <>
       <nav className="fixed top-0 z-50 h-[60px]  w-full bg-white border-b border-gray-200 dark:bg-black dark:border-gray-700">
         <div className="p-[15px] h-full lg:px-5 lg:pl-3 ">
-          <div className="flex items-center justify-between">
+          <div className="flex h-full items-center justify-between">
             <div className="flex items-center justify-start rtl:justify-end">
               <button
                 data-drawer-target="logo-sidebar"
@@ -41,7 +41,22 @@ const Sidebar = () => {
                 </span>
               </Link>
             </div>
+
             <div className="flex items-center">
+              {" "}
+              <div className="flex items-center justify-end mr-40 ">
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  className="w-full  border  border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <button
+                  type="button"
+                  className="ml-2 px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                >
+                  Search
+                </button>
+              </div>
               <div className="flex items-center ms-3">
                 <div>
                   <button
