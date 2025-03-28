2ae2002713e8563855d77f67c003990364b724d1
atharvaombase <aaombase@gmail.com>
2025-03-28 11:03:37 +0530

    Fix:Increased the size of the eye icon in the password field.

--- Frontend/src/pages/Authentication/SignUp.jsx
+++ Frontend/src/pages/Authentication/SignUp.jsx
@@ -1,5 +1,5 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
-import { FcGoogle } from "react-icons/fc";
 import { FiEye, FiEyeOff } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
@@ -40,7 +40,7 @@ const SignUp = () => {
             <button
               type="button"
               onClick={() => setShowPassword(!showPassword)}
-              className="absolute right-3 top-3 text-gray-500 hover:text-gray-700"
+              className="absolute right-3 top-4 text-2xl text-gray-500 hover:text-gray-700"
             >
               {showPassword ? <FiEyeOff /> : <FiEye />}
             </button>
@@ -56,7 +56,7 @@ const SignUp = () => {
             <button
               type="button"
               onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-              className="absolute right-3 top-3 text-gray-500 hover:text-gray-700"
+              className="absolute right-3 top-4 text-2xl text-gray-500 hover:text-gray-700"
             >
               {showConfirmPassword ? <FiEyeOff /> : <FiEye />}
             </button>
