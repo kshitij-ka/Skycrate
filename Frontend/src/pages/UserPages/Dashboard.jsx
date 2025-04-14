4219570d80f9094a29fdd9aecac2c61a6289a740
Atharva Ombase <94031822+atharvaombase@users.noreply.github.com>
2025-04-15 04:14:00 +0530

    Feat:Added file table on the Dashboard

--- Frontend/src/pages/UserPages/Dashboard.jsx
+++ Frontend/src/pages/UserPages/Dashboard.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar";
+import FileList from "../../components/FileList";
 
 const Dashboard = () => {
+  const [files, setFiles] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "http://192.168.29.61:8080/api/hdfs/listFiles?hdfsPath=/"
+      );
+      const data = await response.json();
+      setFiles(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <Sidebar />
       <div className="p-4 sm:ml-64">
         <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg mt-14">
-          <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-          <p>
-            This is your custom dashboard body. Place your widgets, charts,
-            stats, or other components here.
-          </p>
+          <FileList files={files}></FileList>
         </div>
       </div>
     </>
