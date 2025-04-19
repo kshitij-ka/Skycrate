afccce0be34b53476e83bebf4a898623b5c6b657
Atharva Ombase <94031822+atharvaombase@users.noreply.github.com>
2025-04-19 18:56:57 +0530

    Feat:Added the upload reducer in the store for re-renders

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,9 +1,11 @@
 // src/redux/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import pathReducer from "./pathSlice";
+import setIsUploadingReducer from "./UploadStatusSlice";
 
 export const store = configureStore({
   reducer: {
     path: pathReducer,
+    upload: setIsUploadingReducer,
   },
 });
